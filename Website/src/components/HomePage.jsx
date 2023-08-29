99cca96f31ee431a570725cbfc50de325a43c152
root <root@DESKTOP-L7V09QC.localdomain>
2023-08-28 21:05:16 -0500

    Added HomePage file with a welcome message

---
+++ Website/src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+// src/components/HomePage.js
+// src/components/HomePage.jsx
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to Our E-Commerce Store</h1>
+    </div>
+  );
+}
+
+export default HomePage;
+
