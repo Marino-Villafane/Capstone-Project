160ebbc76ea9a610ee9bc7b8e4b58c59cbedea33
root <root@DESKTOP-L7V09QC>
2023-09-28 18:24:49 -0500

    updated backend stuff for adding an admin

--- Website/Backend/db.js
+++ Website/Backend/db.js
@@ -2,11 +2,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',                    // Your database username
-  host: 'localhost', // Your RDS endpoint
-  database: 'postgres',      // Your RDS database name (DB ID)
-  password: 'Messimagical10!',           // Your database password
-  port: 5432,                         // PostgreSQL port (usually 5432)
+  user: 'postgres',                    
+  host: 'localhost', 
+  database: 'postgres',      
+  password: 'Messimagical10!',          
+  port: 5432,                      
 });
 
 module.exports = pool;
--- Website/Backend/src/routes/adminRoutes.js
+++ Website/Backend/src/routes/adminRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {getAdmin, newAdmin}= require("../controllers/adminControllers");
+const {getAdmin, newAdmin, login}= require("../controllers/adminControllers");
 
 router.get("/admin", getAdmin);
 router.post("/admin", newAdmin);
+router.get("/login", login);
 
 module.exports = router;
